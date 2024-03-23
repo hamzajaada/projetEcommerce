1121051af7c8e721edf203cc68dc8c3c827de7cb
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-03-23 22:18:49 +0000

    pushed by abdelilah

--- Projet1/src/main/java/Model/Iplementation/ArticlesDaoimpl.java
+++ Projet1/src/main/java/Model/Iplementation/ArticlesDaoimpl.java
@@ -14,43 +14,43 @@ public class ArticlesDaoimpl {
     //private static Connection con;
     private PreparedStatement stm;
     private ResultSet rs;
-   public List<Article> getAllArticle(){
-       List<Article> Articles = new ArrayList<>();
-       Connection con = null;
-       String request = "select * from articles";
-       try {
-           con = DbConnection.getConnexion();
-           System.out.println("avant stm");
-           stm = con.prepareStatement(request);
-           System.out.println("avant rs");
-           rs = stm.executeQuery();
+    public List<Article> getAllArticle(){
+        List<Article> Articles = new ArrayList<>();
+        Connection con = null;
+        String request = "select * from articles";
+        try {
+            con = DbConnection.getConnexion();
+            System.out.println("avant stm");
+            stm = con.prepareStatement(request);
+            System.out.println("avant rs");
+            rs = stm.executeQuery();
 
-           while(rs.next()) {
-               Article p = new Article(rs.getInt(1),rs.getString(2),rs.getDouble(3), rs.getInt(4),rs.getInt(5),rs.getString(6));
-               System.out.println("while test");
-               Articles.add(p);
-           }
-           System.out.println("apres rs");
-           System.out.println("les articles : " + Articles.toString());
-       } catch (Exception e) {
-           e.printStackTrace();
-       } finally {
-           try {
-               if (rs != null) {
-                   rs.close();
-               }
-               if (stm != null) {
-                   stm.close();
-               }
-               if (con != null) {
-                   con.close();
-               }
-           } catch (SQLException e) {
-               e.printStackTrace();
-           }
-       }
-       return  Articles;
-   }
+            while(rs.next()) {
+                Article p = new Article(rs.getInt(1),rs.getString(2),rs.getDouble(3), rs.getInt(4),rs.getInt(5),rs.getString(6));
+                System.out.println("while test");
+                Articles.add(p);
+            }
+            System.out.println("apres rs");
+            System.out.println("les articles : " + Articles.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stm != null) {
+                    stm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return  Articles;
+    }
     public Article getArticleById(int id) {
         Article article = null;
         Connection con = null;
