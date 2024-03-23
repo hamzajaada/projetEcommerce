bd07b85dd0fb6b80ed78a2d82bee803b4429e77f
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-03-23 15:37:13 +0000

    pushed by abdelilah

--- Projet1/src/main/java/Controller/loginServlet.java
+++ Projet1/src/main/java/Controller/loginServlet.java
@@ -1,15 +1,24 @@
 package Controller;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
 
 import java.io.IOException;
+import java.util.Objects;
 
+@WebServlet(name = "loginServlet", urlPatterns = "/loginServlet")
 public class loginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nom = req.getParameter("nom");
+        String motPasse = req.getParameter("password");
 
+        String adminUsername = getInitParameter("adminUsername");
+        String adminPassword = getInitParameter("adminPassword");
+
+        if(Objects.equals(nom, adminUsername) && Objects.equals(motPasse , adminPassword)) {
+
+        }
     }
 }
