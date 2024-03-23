8656016ca3fda86645be37c1316e3fb31bbff2af
hamzajaada <hamzajaada22@gmail.com>
2024-03-23 15:01:58 +0100

    push

--- Projet1/src/main/java/Model/Client.java
+++ Projet1/src/main/java/Model/Client.java
@@ -1,4 +1,4 @@
-package Classes;
+package Model;
 
 public class Client {
 
---
+++ Projet1/src/main/java/Model/DbConnection.java
@@ -0,0 +1,30 @@
+package Model;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static Connection con;
+    private static String url = "jdbc:mysql://localhost:3306/ecomercejee";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url,"root","");
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Problème de base de données");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problème avec le pilote");
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnexion() {
+        return con;
+    }
+
+}
