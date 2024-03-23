ead0eca2118fed4eb8664e5d5270f11f0e85ef78
AbdelilahElgallati <abdelilah.elgallati6@gamil.com>
2024-03-23 18:11:56 +0000

    pushed by abdelilah

--- Projet1/src/main/java/Model/DbConnection.java
+++ Projet1/src/main/java/Model/DbConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static Connection con;
+    private static Connection con = null;
     private static String url = "jdbc:mysql://localhost:3306/ecomercejee";
 
     static {
@@ -26,5 +26,4 @@ public class DbConnection {
     public static Connection getConnexion() {
         return con;
     }
-
 }
--- Projet1/src/main/java/Model/Iplementation/ArticlesDaoimpl.java
+++ Projet1/src/main/java/Model/Iplementation/ArticlesDaoimpl.java
@@ -11,30 +11,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArticlesDaoimpl {
-    private static Connection con;
+    //private static Connection con;
     private PreparedStatement stm;
     private ResultSet rs;
    public List<Article> getAllArticle(){
-   List<Article> Articles = new ArrayList<Article>();
-        con = DbConnection.getConnexion();
+       List<Article> Articles = new ArrayList<>();
+       Connection con = null;
        String request = "select * from articles";
        try {
+           con = DbConnection.getConnexion();
+           System.out.println("avant stm");
            stm = con.prepareStatement(request);
+           System.out.println("avant rs");
            rs = stm.executeQuery();
+
            while(rs.next()) {
                Article p = new Article(rs.getInt(1),rs.getString(2),rs.getDouble(3), rs.getInt(4),rs.getInt(5),rs.getString(6));
+               System.out.println("while test");
                Articles.add(p);
            }
+           System.out.println("apres rs");
+           System.out.println("les articles : " + Articles.toString());
        } catch (Exception e) {
            e.printStackTrace();
+       } finally {
+           try {
+               if (rs != null) {
+                   rs.close();
+               }
+               if (stm != null) {
+                   stm.close();
+               }
+               if (con != null) {
+                   con.close();
+               }
+           } catch (SQLException e) {
+               e.printStackTrace();
+           }
        }
        return  Articles;
    }
     public Article getArticleById(int id) {
         Article article = null;
-        con = DbConnection.getConnexion();
+        Connection con = null;
         String request = "SELECT * FROM articles WHERE id = ?";
         try {
+            con = DbConnection.getConnexion();
             stm = con.prepareStatement(request);
             stm.setInt(1, id);
             rs = stm.executeQuery();
@@ -43,17 +65,6 @@ public class ArticlesDaoimpl {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (stm != null) {
-                    stm.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
         return article;
     }
